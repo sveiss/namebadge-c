7c12dc4f34923803640f68d8455832aa60ba5d40
Stephen Veiss <stephen@brokenbottle.net>
2021-03-14 22:55:42 +0000

    debug helper

---
+++ debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tmux \
+    new-session -s "pico-gdb" 'openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg' \; \
+    split-window 'gdb-multiarch -ex "target remote localhost:3333" build/namebadge.elf' \; \
+    detach-client
+
+exec tmux attach -t "pico-gdb"
